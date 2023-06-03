3b8d80a06fb3799158745acfc39431b1262561f8
JD Singh <jagdeepsingh@founderandlightning.com>
2023-06-03 05:41:56 +0530

    Add favicon at 5 places [needs optimisation]

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="shortcut icon" href="{{ asset('chirpi.png') }}" type="image/png">
         <title> {{ config('app.name') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        <!-- Favicon -->
-        <link rel="shortcut icon" href="{{ asset('chirpi.png') }}" type="image/png">
 
         <!-- Styles -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
