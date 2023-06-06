84bda67e0da8c37f8efc6b8b35ec64006eabe460
JD Singh <jagdeepsingh@founderandlightning.com>
2023-06-06 15:13:49 +0530

    Make factory use kebab-version of name as url

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -9,12 +9,14 @@ class ClientFactory extends Factory
 {
     public function definition(): array
     {
+        $name = fake()->name();
         return [
-            'name' => fake()->name(),
+            'name' => $name,
             'email' => fake()->safeEmail(),
             'email_verified_at' => now(),
             'password' => bcrypt('client'),
             'remember_token' => Str::random(10),
+            'url' => Str::kebab($name),
         ];
     }
 }
