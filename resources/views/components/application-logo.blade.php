d5fb643083239fdfca308e907f5aca2ad412a097
JD Singh <jagdeepsingh@founderandlightning.com>
2023-06-07 13:05:22 +0530

    Show screen-resolution indicator, for local-dev-env

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,24 @@
 <img src="{{ url('chirpi.png') }}" alt="Chirpi" class="h-20 w-auto" />
+@if(app()->environment('local'))
+<div class="text-gray-500 fixed top-0 inset-x-1/2 cursor-help" title="Screen size indicator">
+    <span class="sm:hidden">
+        mobile[&lt;640px]
+    </span>
+    <span class="hidden sm:block md:hidden">
+        small[&lt;768px]
+    </span>
+    <span class="hidden md:block lg:hidden">
+        medium[&lt;1024px]
+    </span>
+    <span class="hidden lg:block xl:hidden">
+        large[&lt;1280px]
+    </span>
+    <span class="hidden xl:block 2xl:hidden">
+        XLarge[&lt;1536px]
+    </span>
+    <span class="hidden 2xl:block">
+        XxLarge[&gt;1536px]
+    </span>
+</div>
+
+@endif
