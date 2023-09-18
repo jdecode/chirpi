cdb425cfd659e57487d71a948ab66258963f8d57
JD Singh <jagdeepsingh@founderandlightning.com>
2023-09-19 00:50:31 +0530

    Show screen size in bottom-left side of the page

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,23 +1,23 @@
 <img src="{{ url('chirpi.png') }}" alt="Chirpi" class="h-20 w-auto" />
 @if(app()->environment('local'))
-<div class="text-gray-500 fixed top-0 -ml-8 inset-x-1/2 cursor-help" title="Screen size indicator">
+<div class="fixed z-50 left-0 bottom-0 bg-blue-500 p-2 px-4 text-white text-xl font-bold" title="Screen size indicator">
     <span class="sm:hidden">
-        mobile[&lt;640px]
+        default &lt;640px
     </span>
     <span class="hidden sm:block md:hidden">
-        small[&lt;768px]
+        sm &lt;768px
     </span>
     <span class="hidden md:block lg:hidden">
-        medium[&lt;1024px]
+        md &lt;1024px
     </span>
     <span class="hidden lg:block xl:hidden">
-        large[&lt;1280px]
+        lg &lt;1280px
     </span>
     <span class="hidden xl:block 2xl:hidden">
-        XLarge[&lt;1536px]
+        xl &lt;1536px
     </span>
     <span class="hidden 2xl:block">
-        XXLarge[&gt;1536px]
+        2xl &gt;1536px
     </span>
 </div>
 
